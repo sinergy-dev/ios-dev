f9bc2645164bb2453a4793c20b9d03b201c95743
sinergy-dev <appsinergy@gmail.com>
2020-07-20 11:26:57 +0700

    Initial Commit

---
+++ layout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  layout
+//
+//  Created by Rama Agastya on 20/07/20.
+//  Copyright © 2020 Rama Agastya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
