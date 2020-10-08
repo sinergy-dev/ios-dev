2c9300e80cf877b041b9a9e9ecbc38f1550dfe55
rama11 <prof.agastyo@gmail.com>
2020-10-08 14:27:44 +0700

    add controller for job done, with upload file

--- layout/ViewController/DetailJob/JobDoneController.swift
+++ layout/ViewController/DetailJob/JobDoneController.swift
@@ -8,23 +8,100 @@
 
 import UIKit
 
-class JobDoneController: UIViewController {
-
+class JobDoneController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var jobProgressFromSegue:JobList!
+    
+    var imagePicker  = UIImagePickerController()
+    
+    @IBOutlet weak var inputSummary: UITextField!
+    @IBOutlet weak var inputRootCause: UITextField!
+    @IBOutlet weak var inputCounterMeasure: UITextField!
+    
+    @IBOutlet weak var jobDonePicture: UIImageView!
+    @IBOutlet weak var jobDonePicturePickButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        jobDonePicture.isHidden = true
+        imagePicker.delegate = self
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func jobDonePicturePick(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
     }
-    */
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            jobDonePicture.image = image
+            jobDonePicture.isHidden = false
+            jobDonePicturePickButton.isHidden = true
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func jobDoneSubmit(_ sender: Any) {
+        let dialogMessage = UIAlertController(title: "Job Done", message: "Are you sure to submit this job?", preferredStyle: .alert)
+        
+        let submit = UIAlertAction(title: "Submit", style: .default, handler: { (action) -> Void in
+            print("Ok button tapped")
+            self.submitJobDone(image: self.jobDonePicture.image!){
+                _ = self.navigationController?.popViewController(animated: true)
+            }
+        })
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel){ (action) -> Void in
+            print("Cancel button tapped")
+        }
+        
+        dialogMessage.addAction(submit)
+        dialogMessage.addAction(cancel)
+        
+        self.present(dialogMessage, animated: true, completion: nil)
+    }
+    
+    func submitJobDone( image:UIImage ,completed: @escaping () -> ()){
+        let url = GlobalVariable.urlUpdateJobDone
+
+        var components = URLComponents(string: url)!
+        components.queryItems = [
+            URLQueryItem(name: "id_job", value: String(jobProgressFromSegue!.id)),
+            URLQueryItem(name: "job_summary", value: inputSummary.text),
+            URLQueryItem(name: "job_rootcause", value: inputRootCause.text),
+            URLQueryItem(name: "job_countermeasure", value: inputCounterMeasure.text)
+        ]
+        components.percentEncodedQuery = components.percentEncodedQuery?.replacingOccurrences(of: "+", with: "%2B")
+        
+        let filename = "job_report.jpeg"
+        let boundary = UUID().uuidString
+
+        var request = URLRequest(url:components.url!)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+        request.setValue(UserDefaults.standard.string(forKey: "Token")!, forHTTPHeaderField: "Authorization")
+        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+
+        var data = Data()
+        
+        data.append("\r\n--\(boundary)\r\n".data(using: .utf8)!)
+        data.append("Content-Disposition: form-data; name=\"job_documentation\"; filename=\"\(filename)\"\r\n".data(using: .utf8)!)
+        data.append("Content-Type: image/jpeg\r\n\r\n".data(using: .utf8)!)
+        data.append(image.jpegData(compressionQuality: 1)!)
+        data.append("\r\n--\(boundary)--\r\n".data(using: .utf8)!)
+        
+        URLSession.shared.uploadTask(with: request, from: data) { (data, response, error) in
+            if error == nil {
+                DispatchQueue.main.async {
+                    completed()
+                }
+            }
+        }.resume()
+    }
+    
+    
 
 }
